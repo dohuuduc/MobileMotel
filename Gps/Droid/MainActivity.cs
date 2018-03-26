9481a29b5650e3691c9ae7699abe25717a48fc2a
luulong83 <luucangkimlong83@yahoo.com.vn>
2018-03-26 16:45:59 +0700

    a
    a

---
+++ Gps/Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace TodoWCF.Droid
+{
+    [Activity(Label = "TodoWCF.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        internal static MainActivity Instance { get; private set; }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Instance = this;
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            App.Speech = new Speech();
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ Gps/Droid/obj/Debug/android/src/md572070c8b08e45781bbc4a4a640016867/Speech.java
@@ -0,0 +1,48 @@
+package md572070c8b08e45781bbc4a4a640016867;
+
+
+public class Speech
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer,
+		android.speech.tts.TextToSpeech.OnInitListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onInit:(I)V:GetOnInit_IHandler:Android.Speech.Tts.TextToSpeech/IOnInitListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("TodoWCF.Droid.Speech, TodoWCF.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Speech.class, __md_methods);
+	}
+
+
+	public Speech ()
+	{
+		super ();
+		if (getClass () == Speech.class)
+			mono.android.TypeManager.Activate ("TodoWCF.Droid.Speech, TodoWCF.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onInit (int p0)
+	{
+		n_onInit (p0);
+	}
+
+	private native void n_onInit (int p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
